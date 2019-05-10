de7960ae1dabe608e6fd909419d67e66c4b0e568
Natasha Hakun <natasha.hakun@ey.com>
2019-05-10 17:21:50 -0400

    Update button styles to account for icon button - remove liked gif

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import './Button.scss';
 
-export const Button = ({ children, disabled, icon, type, ...props }) => 
-    <button className="button" disabled={disabled} type={type} {...props}>{ children }</button>
+export const Button = ({ buttonType, children, disabled, icon, type, ...props }) => 
+    <button className={classNames('button', { 'button--secondary': buttonType === 'secondary', 'button--icon': buttonType === 'icon' })} disabled={disabled} type={type} {...props}>{ children }</button>
 
 Button.defaultProps = {
+    buttonType: 'primary',
     disabled: false,
     icon: '',
     type: 'button'
 };
 
 Button.propTypes = {
+    buttonType: PropTypes.oneOf(['primary', 'secondary', 'icon']),
     children: PropTypes.oneOfType([
         PropTypes.arrayOf(PropTypes.element),
         PropTypes.element,
