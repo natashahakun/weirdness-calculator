cc9be850794a99fc3194e58142c80b4a8f761378
Natasha Hakun <natasha.hakun@ey.com>
2019-05-10 17:25:24 -0400

    Enable removing Gif from Liked

--- src/components/Gif/Gif.jsx
+++ src/components/Gif/Gif.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Button } from '../../components';
 import './Gif.scss';
 
-export const Gif = ({ title, url }) =>
+export const Gif = ({ removeFunc, title, url }) =>
     <div className="gif">
         <h3 className="gif__heading">{ title }</h3>
-        <img className="gif__image" src={url} alt={`Giphy result ${title}`} />
+
+        <div className="gif__image-wrapper">
+            <img className="gif__image" src={url} alt={`Giphy result ${title}`} />
+
+            { removeFunc &&
+                <div className="gif__action">
+                    <Button buttonType="icon" onClick={removeFunc} type="type">
+                        X
+                    </Button>
+                </div>
+            }
+        </div>
     </div>
 
+Gif.defaultProps = {
+    removeFunc: null
+}
+
 Gif.propTypes = {
+    removeFunc: PropTypes.func || null,
     title: PropTypes.string.isRequired,
     url: PropTypes.string.isRequired
 };
--- src/components/Liked/Liked.jsx
+++ src/components/Liked/Liked.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, Gif, Title } from '../../components';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { removeLiked } from '../../actions/liked.actions';
 import './Liked.scss';
 
 const LikedSFC = ({ liked, removeLiked }) =>
@@ -12,7 +14,7 @@ const LikedSFC = ({ liked, removeLiked }) =>
             <div className="liked__gifs">{ liked.map((liked) => {
                 return (
                     <div key={liked.url}>
-                        <Gif title={liked.title} url={liked.url} />
+                        <Gif removeFunc={() => removeLiked(liked)} title={liked.title} url={liked.url} />
                     </div>
                 )
              } ) }</div>
@@ -25,11 +27,16 @@ LikedSFC.propTypes = {
         title: PropTypes.string,
         url: PropTypes.string
         })
-    )
+    ),
+    removeLiked: PropTypes.func
 }
 
 const mapStateToProps = ({ liked }) => ({
     liked
 });
 
-export const Liked = connect(mapStateToProps)(LikedSFC);
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+    removeLiked
+}, dispatch);
+
+export const Liked = connect(mapStateToProps, mapDispatchToProps)(LikedSFC);
